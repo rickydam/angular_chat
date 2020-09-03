1ddb40eded8a06b7ec43a23582283741628d77af
RenatoSerk <brickworked@gmail.com>
2020-09-03 15:55:48 -0400

    Add socket library                                                       Added socket.io library

--- socket-backend/server.js
+++ socket-backend/server.js
@@ -1,7 +1,13 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => res.send('hello!'));
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
 http.listen(3000, () => {
     console.log('listening on *:3000');
 });
