37debc7eb80e38ac47d7e76d925bfd40d5c814df
Ricky Dam <RickyDamOttawa@gmail.com>
2020-09-09 18:01:50 -0400

    Update node server to load angular compiled files

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-//require('dotenv').config();
-const app = require('express')();
+const express = require('express');
+const app = express();
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'dist/socket-frontend')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/socket-frontend/index.html'));
+});
+
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => res.send('hello!'));
-
+const io = require('socket.io')(http);
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('message', (msg) => {
@@ -13,7 +16,6 @@ io.on('connection', (socket) => {
         socket.broadcast.emit('message-broadcast', msg);
     });
 });
-
 http.listen(port, () => {
     console.log('listening on :' + port.toString())
 });
